13787010f6805da47f70b5d98c0d2ab76de4ba2f
liohumb <helloliohumb@gmail.com>
2023-03-02 12:00:21 +0100

    installation des dépendances et mise en place de react redux

--- src/index.js
+++ src/index.js
@@ -1,14 +1,37 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import {configureStore} from '@reduxjs/toolkit'
+import {Provider} from 'react-redux'
+import {persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+import {PersistGate} from 'redux-persist/integration/react'
 
-import App from './App'
+import authReducer from 'state'
+import App from 'App'
+
+const persistConfig = {key: "root", storage, version: 1}
+const persistedReducer = persistReducer(persistConfig, authReducer)
+const store = configureStore({
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) => {
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
+            }
+        })
+    }
+})
 
 const root = ReactDOM.createRoot( document.getElementById( 'root' ) )
 root.render(
     <React.StrictMode>
-        <BrowserRouter>
-            <App/>
-        </BrowserRouter>
+        <Provider store={store}>
+            <PersistGate loading={null} persistor={persistStore(store)}>
+                <BrowserRouter>
+                    <App/>
+                </BrowserRouter>
+            </PersistGate>
+        </Provider>
     </React.StrictMode>
 )
