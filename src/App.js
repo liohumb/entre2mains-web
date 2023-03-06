e2b334b7d8b85f6bfc16a19802b1a6366b250375
liohumb <helloliohumb@gmail.com>
2023-03-06 11:39:50 +0100

    correction emplacement header component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import {Routes, Route, BrowserRouter} from 'react-router-dom'
 
-import Header from 'navigations/header/Header'
-import Footer from 'navigations/footer/Footer'
 import Home from 'pages/home/Home'
 import Register from 'authentification/register/Register'
 import Login from 'authentification/login/Login'
@@ -13,14 +11,12 @@ function App() {
     return (
         <>
             <BrowserRouter>
-                <Header/>
                 <Routes>
                     <Route path="/profil/:userId" element={<User/>}/>
                     <Route path="/connexion" element={<Login/>}/>
                     <Route path="/inscription" element={<Register/>}/>
                     <Route index path="/" element={<Home/>}/>
                 </Routes>
-                <Footer/>
             </BrowserRouter>
         </>
     )
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,14 @@
+import Header from '../../navigations/header/Header'
+
 import './home.scss'
 
 export default function Home(  ) {
     return (
-        <section className="Home">
-            <h1>HOME</h1>
-        </section>
+        <>
+            <Header/>
+            <section className="Home">
+                <h1>HOME</h1>
+            </section>
+        </>
     )
 }
