b4bbb3b44f7d5c94d71e7989267f76ed7d881bb6
liohumb <helloliohumb@gmail.com>
2023-03-06 10:16:31 +0100

    mise en place de la barre de navigation provisoire

--- src/navigations/header/Header.jsx
+++ src/navigations/header/Header.jsx
@@ -1,9 +1,43 @@
+import {Link} from 'react-router-dom'
 import './header.scss'
 
-export default function Header(  ) {
+export default function Header() {
     return (
-        <>
-
-        </>
+        <header className="header">
+            <Link to="/" className="header__logo">Entre 2 Mains</Link>
+            <nav className="header__menu">
+                <form className="header__menu-search">
+                    <label htmlFor="search">
+                    </label>
+                    <input type="text" name="search" id="search" placeholder="Rechercher…"/>
+                    <button type="submit">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                            <path d="M10 18a7.952 7.952 0 0 0 4.897-1.688l4.396 4.396 1.414-1.414-4.396-4.396A7.952 7.952 0 0 0 18 10c0-4.411-3.589-8-8-8s-8 3.589-8 8 3.589 8 8 8zm0-14c3.309 0 6 2.691 6 6s-2.691 6-6 6-6-2.691-6-6 2.691-6 6-6z"/>
+                        </svg>
+                    </button>
+                </form>
+                <span className="header__menu-link">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                        <path d="M12 11.807A9.002 9.002 0 0 1 10.049 2a9.942 9.942 0 0 0-5.12 2.735c-3.905 3.905-3.905 10.237 0 14.142 3.906 3.906 10.237 3.905 14.143 0a9.946 9.946 0 0 0 2.735-5.119A9.003 9.003 0 0 1 12 11.807z"/>
+                    </svg>
+                </span>
+                <span className="header__menu-link">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                        <path d="M7 7h10v2H7zm0 4h7v2H7z"/>
+                        <path d="M20 2H4c-1.103 0-2 .897-2 2v18l5.333-4H20c1.103 0 2-.897 2-2V4c0-1.103-.897-2-2-2zm0 14H6.667L4 18V4h16v12z"/>
+                    </svg>
+                </span>
+                <span className="header__menu-link">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                        <path d="M12 22a2.98 2.98 0 0 0 2.818-2H9.182A2.98 2.98 0 0 0 12 22zm7-7.414V10c0-3.217-2.185-5.927-5.145-6.742C13.562 2.52 12.846 2 12 2s-1.562.52-1.855 1.258C7.185 4.074 5 6.783 5 10v4.586l-1.707 1.707A.996.996 0 0 0 3 17v1a1 1 0 0 0 1 1h16a1 1 0 0 0 1-1v-1a.996.996 0 0 0-.293-.707L19 14.586z"/>
+                    </svg>
+                </span>
+                <span className="header__menu-link">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                        <path d="M12 4C9.243 4 7 6.243 7 9h2c0-1.654 1.346-3 3-3s3 1.346 3 3c0 1.069-.454 1.465-1.481 2.255-.382.294-.813.626-1.226 1.038C10.981 13.604 10.995 14.897 11 15v2h2v-2.009c0-.024.023-.601.707-1.284.32-.32.682-.598 1.031-.867C15.798 12.024 17 11.1 17 9c0-2.757-2.243-5-5-5zm-1 14h2v2h-2z"/>
+                    </svg>
+                </span>
+            </nav>
+        </header>
     )
 }
---
+++ src/navigations/nav/Nav.jsx
@@ -0,0 +1,8 @@
+import './nav.scss'
+
+export default function Nav() {
+    return (
+        <div className="nav">
+        </div>
+    )
+}
